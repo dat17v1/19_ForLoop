b570d722a923051704d5fe74855bba40201ad532
clbokea <clbo@kea.dk>
2017-03-06 11:00:15 +0100

    code added

---
+++ Code/Loop.java
@@ -0,0 +1,22 @@
+public class Loop
+{
+    public static void main(String[] args)
+    {
+        // declarere en variable
+        //int i = 0;
+
+        // Betingelse
+        //i <= 10
+
+        // Ændring a variablen i
+        // i + 1
+
+        for(int i = 0; i <= 10; i = i+1)
+        {
+            // udfør denne kode for hver iteration
+            System.out.println("Hello World nummer " + i);
+        }
+
+        System.out.println("Hello World nummer er slut");
+    }
+}
